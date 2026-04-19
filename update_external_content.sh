5660d589a5f7b47fca4ae919f221cbb68a0c6086
Keegan Green <keeganmjgreen@gmail.com>
2026-04-18 22:20:26 -0400

    update python-cluedo submodule

--- update_external_content.sh
+++ update_external_content.sh
@@ -1,3 +1,4 @@
 git submodule update --recursive --remote
 rm external/how_phasors_work/myst.yml
 rm external/ontario_electricity_market/myst.yml
+cd external/python-cluedo/ && git checkout for-blog && git reset --hard origin/for-blog && cd ../../ && git add external/python-cluedo/
