d4244f9da0043753424c53b71ca56df3c8f670bf
Keegan Green <keeganmjgreen@gmail.com>
2026-07-29 19:56:28 -0400

    update submodules

--- update_external_content.sh
+++ update_external_content.sh
@@ -1,4 +1,5 @@
 git submodule update --recursive --remote
 rm external/how_phasors_work/myst.yml
+cd external/ontario_electricity_market/ && git checkout version-for-blog && git reset --hard origin/version-for-blog && cd ../../ && git add external/ontario_electricity_market/
 rm external/ontario_electricity_market/myst.yml
 cd external/python-cluedo/ && git checkout for-blog && git reset --hard origin/for-blog && cd ../../ && git add external/python-cluedo/
