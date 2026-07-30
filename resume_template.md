d2f7522aae07189c0290f6cc6b612e7566e16941
Keegan Green <keeganmjgreen@gmail.com>
2026-07-29 23:56:00 -0400

    update resume programmatically

---
+++ resume_template.md
@@ -0,0 +1,10 @@
+---
+edit_url: null
+downloads: []
+---
+
+<div class="col-page-right" style="max-width: calc(8.5in/0.9)">
+<div class="resume">
+<!-- subtemplate -->
+</div>
+</div>
--- update_external_content.sh
+++ update_external_content.sh
@@ -3,3 +3,22 @@ rm external/how_phasors_work/myst.yml
 cd external/ontario_electricity_market/ && git checkout version-for-blog && git reset --hard origin/version-for-blog && cd ../../ && git add external/ontario_electricity_market/
 rm external/ontario_electricity_market/myst.yml
 cd external/python-cluedo/ && git checkout for-blog && git reset --hard origin/for-blog && cd ../../ && git add external/python-cluedo/
+cd external/bluegreen_resume_template/ && git checkout green-resume && git reset --hard origin/green-resume & cd ../../ && git add external/bluegreen_resume_template/
+
+python3 -c '
+import sys
+with open("resume_template.md") as f:
+    content = f.read()
+with open("external/bluegreen_resume_template/populated_subtemplate.html") as f:
+    populated_subtemplate = f.read()
+REPLACEMENTS = [
+    (">keeganmjgreen@gmail.com<", ">Email<"),
+    (" <br> ", " "),
+    ("<br>", " "),
+]
+for (old, new) in REPLACEMENTS:
+    populated_subtemplate = populated_subtemplate.replace(old, new)
+content = content.replace("<!-- subtemplate -->", populated_subtemplate)
+with open("resume.md", "w") as f:
+    f.write(content)
+'
